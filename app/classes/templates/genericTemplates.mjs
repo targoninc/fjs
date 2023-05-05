a80ddb99a662156b8b3406dbe10bc11074316744
alexa <alex@trirecords.eu>
2023-05-06 00:17:50 +0200

    add child template to prove it works

--- app/classes/templates/genericTemplates.mjs
+++ app/classes/templates/genericTemplates.mjs
@@ -1,14 +1,20 @@
 import {FJS} from "../../../framework/f.mjs";
 
 export class GenericTemplates {
+    static testChild = () => {
+        return FJS.create("span")
+            .classes('border')
+            .text('test child').build();
+    };
     static test = testClass => {
         return FJS.create("span")
-            .classes(testClass)
+            .classes(testClass, 'border')
             .attributes('data-test', 'test', 'data-test2', 'test2')
             .text('test text')
             .onclick(() => {
                 console.log('test');
             })
+            .children(GenericTemplates.testChild())
             .build();
-    }
+    };
 }
